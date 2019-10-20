75a7832fa10d7d9ddd1f0695d62c2341bc0579f6
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-10-20 16:03:28 +0300

    Fix the version folder error in release-docs.sh

--- release-docs.sh
+++ release-docs.sh
@@ -5,6 +5,7 @@ docfx ./PatternMatching.Docs/docfx.json
 
 SOURCE_DIR=$PWD
 TEMP_REPO_DIR=$PWD/../PatternMatching-gh-pages
+VERSION=v1.2.0
 
 echo "Removing the temporary doc directory $TEMP_REPO_DIR"
 rm -rf $TEMP_REPO_DIR
@@ -13,14 +14,20 @@ mkdir $TEMP_REPO_DIR
 echo "Cloning the repo with the gh-pages branch"
 git clone https://github.com/TolikPylypchuk/PatternMatching.git --branch gh-pages $TEMP_REPO_DIR
 
-echo "Clear repo directory"
+echo "Clearing the docs directory"
+
 cd $TEMP_REPO_DIR
-git rm -r v1.2.0/*
+
+if [ ! -d "$VERSION" ]; then
+  mkdir $VERSION
+fi
+
+git rm -r $VERSION/*
 
 echo "Copying documentation into the repo"
 cp -r $SOURCE_DIR/docs/* .
 
 echo "Pushing the new docs to the remote branch"
 git add . -A
-git commit -m "Update generated documentation for version 1.2.0"
+git commit -m "Update generated documentation for version $VERSION"
 git push origin gh-pages
