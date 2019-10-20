a982c506f2ca0fb19aaa7dc8cc172177b42ee4f8
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-10-20 15:49:26 +0300

    Configure generating docs as part of the CI pipeline
    Remove the docs folder. Add the release-docs.sh file which will
    generate the docs and push them into the gh-pages branch. Update
    appveyor.yml to run the script on build.

---
+++ release-docs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+docfx ./PatternMatching.Docs/docfx.json
+
+SOURCE_DIR=$PWD
+TEMP_REPO_DIR=$PWD/../PatternMatching-gh-pages
+
+echo "Removing the temporary doc directory $TEMP_REPO_DIR"
+rm -rf $TEMP_REPO_DIR
+mkdir $TEMP_REPO_DIR
+
+echo "Cloning the repo with the gh-pages branch"
+git clone https://github.com/myaccount/my-project.git --branch gh-pages $TEMP_REPO_DIR
+
+echo "Clear repo directory"
+cd $TEMP_REPO_DIR
+git rm -r v1.2.0/*
+
+echo "Copying documentation into the repo"
+cp -r $SOURCE_DIR/docs/* .
+
+echo "Pushing the new docs to the remote branch"
+git add . -A
+git commit -m "Update generated documentation for version 1.2.0"
+git push origin gh-pages
